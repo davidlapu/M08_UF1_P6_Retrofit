18ffa661f60c6bb33607657c6014cce4d8f43f23
David Lama Pulido <david.lama.7e3@itb.cat>
2020-12-02 18:18:11 +0100

    Initial commit

---
+++ app/src/main/java/cat/itb/m08_uf1_p6_retrofit/MainActivity.java
@@ -0,0 +1,14 @@
+package cat.itb.m08_uf1_p6_retrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
